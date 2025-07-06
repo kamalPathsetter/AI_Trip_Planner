e1355caa9a1351c432ecccfcdaca73f3843ec420
Kamal <kamal@RACs-MacBook-Pro.local>
2025-07-06 21:53:46 +0530

    Weather tool done

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -7,18 +7,31 @@ from tools.place_search_tool import PlaceSearchTool
 from tools.expense_calculator_tool import CalculatorTool
 from tools.currency_conversion_tool import CurrencyConverterTool
 
-
 class GraphBuilder():
-    def __init__(self):
-        self.tools = [
-            WeatherInfoTool(),
-            PlaceSearchTool(),
-            CalculatorTool(),
-            CurrencyConverterTool()
-        ]
+    def __init__(self,model_provider: str = "groq"):
+        self.model_loader = ModelLoader(model_provider=model_provider)
+        self.llm = self.model_loader.load_llm()
+        
+        self.tools = []
+        
+        self.weather_tools = WeatherInfoTool()
+        self.place_search_tools = PlaceSearchTool()
+        self.calculator_tools = CalculatorTool()
+        self.currency_converter_tools = CurrencyConverterTool()
+        
+        self.tools.extend([* self.weather_tools.weather_tool_list, 
+                           * self.place_search_tools.place_search_tool_list,
+                           * self.calculator_tools.calculator_tool_list,
+                           * self.currency_converter_tools.currency_converter_tool_list])
+        
+        self.llm_with_tools = self.llm.bind_tools(tools=self.tools)
+        
+        self.graph = None
+        
         self.system_prompt = SYSTEM_PROMPT
-
-    def agent_function(self, state: MessagesState):
+    
+    
+    def agent_function(self,state: MessagesState):
         """Main agent function"""
         user_question = state["messages"]
         input_question = [self.system_prompt] + user_question
@@ -26,14 +39,15 @@ class GraphBuilder():
         return {"messages": [response]}
 
     def build_graph(self):
-        graph_builder = StateGraph(MessagesState)
+        graph_builder=StateGraph(MessagesState)
+        
         graph_builder.add_node("agent", self.agent_function)
         graph_builder.add_node("tools", ToolNode(tools=self.tools))
-        graph_builder.add_edge(START, "agent")
-        graph_builder.add_conditional_edges("agent", tools_condition)
-        graph_builder.add_edge("tools", "agent")
-        graph_builder.add_edge("agent", END)
-        
+        graph_builder.add_edge(START,"agent")
+        graph_builder.add_conditional_edges("agent",tools_condition)
+        graph_builder.add_edge("tools","agent")
+        graph_builder.add_edge("agent",END)
+
         self.graph = graph_builder.compile()
         return self.graph
         
